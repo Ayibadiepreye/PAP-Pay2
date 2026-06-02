f5c8353cb871dd5e9e802571db0bb4635b178735
Ayibadiepreye <bonnieprincewill6@gmail.com>
2026-06-02 22:56:13 +0100

    pap

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -1,6 +1,12 @@
 import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import * as schema from "./schema";
+import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+dotenv.config({ path: path.join(__dirname, "../../../.env") });
 
 const { Pool } = pg;
 
